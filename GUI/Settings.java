595692b2e37d3f8d4ef2f24c3ba36ab9c5db519a
AmirAliyev <64432548+AmirAliyev@users.noreply.github.com>
2021-07-29 23:57:32 +0300

    GUIs 1st version

--- GUI/Settings.java
+++ GUI/Settings.java
@@ -1,4 +1,4 @@
-/*
+package GUI;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -351,7 +351,7 @@ public class Settings extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
     	if ( evt.getSource() == jButton1 )
         {  
-            MainPageGui f = new MainPageGui();
+            MainMenu f = new MainMenu();
             f.setVisible(true);
             setVisible(false);
         }
