faf036ae1af8433e946d57b3d5edb39822717f61
Eren Karakaş <69261887+ErenKarakas1@users.noreply.github.com>
2021-07-30 20:23:27 +0300

    Make Panel url changeable

--- GUI/BrowserPanel.java
+++ GUI/BrowserPanel.java
@@ -9,26 +9,25 @@ import java.awt.*;
 
 public class BrowserPanel {
 
-    public static JPanel makePanel() {
-        Browser browser = EmbeddedBrowser.openURL( "https://www.migros.com.tr/" );
+    public static JPanel makePanel( String url ) {
+        Browser browser = EmbeddedBrowser.openURL( url );
         JPanel panel = new JPanel();
 
         SwingUtilities.invokeLater( () -> {
-            // Creating Swing component for rendering web content
-            // loaded in the given Browser instance.
+            /* Creating Swing component for rendering web content
+            */ loaded in the given Browser instance.
             BrowserView view = BrowserView.newInstance( browser );
 
-            // Creating and displaying Swing app panel.
-            // Close Engine and onClose app window
-            JTextField addressBar = new JTextField( "https://www.migros.com.tr/" );
+            // Creating and displaying Swing app panel
+            JTextField addressBar = new JTextField( url );
             addressBar.addActionListener(e ->
-                    browser.navigation().loadUrl(addressBar.getText()));
-            panel.add(addressBar, BorderLayout.NORTH);
-            panel.add(view, BorderLayout.CENTER);
-            panel.setSize(800, 500);
-            panel.setVisible(true);
+                    browser.navigation().loadUrl( addressBar.getText() ) );
+            panel.add( addressBar, BorderLayout.NORTH );
+            panel.add( view, BorderLayout.CENTER );
+            panel.setSize( 800, 500 );
+            panel.setVisible( true );
 
-            browser.navigation().loadUrl(addressBar.getText());
+            browser.navigation().loadUrl( addressBar.getText() );
 
         });
         return panel;
