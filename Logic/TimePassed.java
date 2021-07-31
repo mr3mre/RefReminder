9a38fa670701d6bd705cc39acacc4f49d14bfec8
Eren Karakaş <69261887+ErenKarakas1@users.noreply.github.com>
2021-07-31 22:25:50 +0000

    Update TimePassed.java

--- Logic/TimePassed.java
+++ Logic/TimePassed.java
@@ -3,8 +3,6 @@ package Logic;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Clock;
-import java.time.ZoneId;
 import java.util.Date;
 import java.util.Scanner;
 
